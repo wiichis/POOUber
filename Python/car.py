8b13f5f106a9fed3acef5415cf1aec284428cfef
Wilson Montenegro <wilmontenegro@me.com>
2020-04-24 12:21:58 -0500

    Declarando metodo constructor para Python

--- Python/car.py
+++ Python/car.py
@@ -1,5 +1,11 @@
+from account import Account
+
 class Car:
     id        = int
     licence   = str
     driver    = str
     passenger = str
+
+    def __init__(self, licence, driver):
+        self.licence  = licence
+        self.driver   = driver

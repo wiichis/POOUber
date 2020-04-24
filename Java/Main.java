1c2960ab9e968bd0df4e02ad7b17c672b3db1968
Wilson Montenegro <wilmontenegro@me.com>
2020-04-23 19:30:04 -0500

    Creacion de Objetos en JavaScript

--- Java/Main.java
+++ Java/Main.java
@@ -1,16 +1,12 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
         car.passenger = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "QW5647";
-        car2.driver = "Andrea Herrera";
-        car2.passenger = 4;
+        Car car2 = new Car("QW5647", new Account("Andrea Herrera", "ANDA876"));
+        car2.passenger = 3;
         car2.printDataCar();
 
     }

b5341a6c34f9e869fb4452c2eb53b134cc9cb9f6
Wilson Montenegro <wilmontenegro@me.com>
2020-04-27 18:50:32 -0500

    Encapsulamiento Java

--- Java/Car.java
+++ Java/Car.java
@@ -2,14 +2,31 @@ class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passenger;
+    private Integer passenger;
 
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
+        
     }
 
     void printDataCar() {
-        System.out.println("License: " + license + " Name Driver: " + driver.name);
+        if(passenger != null){
+        System.out.println("License: " + license + " Name Driver: " + driver.name + " Passengers: " + passenger);
+       }
     }
+
+    public Integer getPassenger(){
+        return passenger;
+    }
+
+    public void setPassenger(Integer passenger){
+        if(passenger ==4){
+        this.passenger = passenger;
+        }else{
+            System.out.println("Necesitas asignar 4 pasajeros");
+        }
+    }
+
+
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,13 +1,13 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
-        car.passenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic");
+        uberX.setPassenger(3);
+        uberX.printDataCar();
 
-        Car car2 = new Car("QW5647", new Account("Andrea Herrera", "ANDA876"));
+        /*Car car2 = new Car("QW5647", new Account("Andrea Herrera", "ANDA876"));
         car2.passenger = 3;
-        car2.printDataCar();
+        car2.printDataCar();*/
 
     }
 }
